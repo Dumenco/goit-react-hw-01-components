848b202f81a452184ace87866fb6d832c126d764
Dumenco <tt-3484@ukr.net>
2020-04-26 10:57:37 +0300

    Updates

---
+++ precache-manifest.b91967fec8c776b8507f847c57911afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6753a0ef673dcd783a3f7448f4cba2b",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "52871777f9d13ddb6918",
+    "url": "/goit-react-hw-01-components/static/css/main.03297946.chunk.css"
+  },
+  {
+    "revision": "10f04287f9db07079627",
+    "url": "/goit-react-hw-01-components/static/js/2.8a6d8d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.8a6d8d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52871777f9d13ddb6918",
+    "url": "/goit-react-hw-01-components/static/js/main.6fd68fe6.chunk.js"
+  },
+  {
+    "revision": "f2ebcb926d6c2cbee00b",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.62340070.js"
+  }
+]);
